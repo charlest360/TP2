9e9a71c5172e108eccea8160cd4b62f31be349c4
charlest360 <charlo-patato@hotmail.com>
2020-05-02 09:28:06 -0400

    appelDeFIlmServiceDansHomeVue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,37 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to our movie database (OMDB)"/>
+    <h1>home</h1>
+    <span class="error in Home.vue :">{{error}}</span>
+    <hello-world :filmsData=filmsData />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-
+import FilmService from '@/services/FilmService.js';
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      filmsData: null,
+      error: null,
+    }
+  },
+  created () {
+    
+    FilmService.getFilms()
+      .then(response => {
+        this.filmsData = response.data;
+      })
+      .catch(error => {
+        this.error = error;
+      });
+
+    
+   
   }
 }
 </script>
