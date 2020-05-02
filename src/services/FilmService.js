d1f83d5070e00b914b1eeaefa326b4209a2af3f7
charlest360 <charlo-patato@hotmail.com>
2020-05-02 09:27:13 -0400

    AjoutDesServicesDeBasePourFilmService

---
+++ src/services/FilmService.js
@@ -0,0 +1,23 @@
+import axios from 'axios'
+
+const apiFilms = axios.create({  
+    baseURL: `http://radiant-plains-67953.herokuapp.com/api`,
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+
+  
+
+  export default {
+    getFilms() {
+      return apiFilms.get('/films');
+    },
+    getFilmsPage(pageNb) {
+        return apiFilms.get('/films?page='+pageNb);
+    },
+   getFilmById(id){
+       return apiFilms.get('/films/'+id);
+   }
+  }
