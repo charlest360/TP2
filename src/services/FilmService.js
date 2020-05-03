9ff4f6c454ac7519c278bc73cd3c17caf4b1142a
charlest360 <charlo-patato@hotmail.com>
2020-05-03 16:57:44 -0400

    ajoutRouteGetFilmsByKeyword

--- src/services/FilmService.js
+++ src/services/FilmService.js
@@ -15,12 +15,15 @@ const apiFilms = axios.create({
       return apiFilms.get('/films');
     },
     getFilmsPage(pageNb) {
-        return apiFilms.get('/films?page='+pageNb);
+      return apiFilms.get('/films?page='+pageNb);
     },
-   getFilmById(id){
-       return apiFilms.get('/films/'+id);
-   },
-   getFilmActors(id){
-    return apiFilms.get('/films/'+id+'/actors');
-}
+    getFilmById(id){
+      return apiFilms.get('/films/'+id);
+    },
+    getFilmActors(id){
+      return apiFilms.get('/films/'+id+'/actors');
+    },
+    getFilmsByKeyword(keyword,pageNb) {
+      return apiFilms.get('/films?keyword='+keyword+'&page='+pageNb);
+    }
   }
