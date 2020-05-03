1664b7ddb653b6e6b5ee7cb8ee8988040c54be63
charlest360 <charlo-patato@hotmail.com>
2020-05-03 16:54:35 -0400

    ajoutCOmponentFilmImage

---
+++ src/components/FilmImage.vue
@@ -0,0 +1,33 @@
+<template>
+    <div v-if="filmInfos.image != null  && filmInfos.image.length >0 ">
+        <img :src="filmInfos.image" alt="une image" :height="height" :width="width">
+    </div>
+            
+    <div v-else>
+        <img src="@/assets/noImage.png" alt="aucune image pour le film" :height="height" :width="width"> 
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            filmInfos: {
+                type: Object,
+                default: null,
+                required : true
+            },
+            height : {
+                type: Number,
+                required : true
+            },
+            width : {
+                type: Number,
+                required : true
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
