dd648b1344c5a606fae3004d612a97d189263f8b
charlest360 <charlo-patato@hotmail.com>
2020-05-03 17:03:31 -0400

    implementationPlusieursCOmponents

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,30 @@
 <template>
   <div class="page">
-    tit
+  
      <div class="home">
-      <h1>Welcome to Our Movie Database!</h1>
-      <span class="error in Home.vue :">{{error}}</span>
-      <home-movies-list :filmsData=filmsData />
+        <h1 v-bind:class='{ hidden: isClicked}'>Welcome to Our Movie Database!</h1>
+        <span class="error in Home.vue :">{{error}}</span>
+        
+        <span class="static" v-bind:class='{ hidden: isClicked}' >Search a movie by keyword: <input v-model="filterName" /></span> 
+        
+        <button  class="static" v-bind:class='{ hidden: isClicked}'  @click="setIsClicked"> Rechercher</button>
+        
+        <button  class="static" v-bind:class='{ hidden: isClicked ==false }' @click="setIsClicked"> Back</button>
+      
+        <div class="movieData">
+          <section class="static" v-bind:class='{ hidden: isClicked == false }' >
+            <film-list :keyword="filterName" :clicked="isClicked" />
+          </section>
+
+        <section class="static" v-bind:class='{ hidden: shouldClose ==true }'>
+          <film-suggestion-list :keyword="filterName" />
+        </section>
+      
+        <section class="static" v-bind:class='{ hidden: isClicked}' >
+          <home-movies-list :filmsData=filmsData />
+        </section>
+      </div>
+      
     </div>
   </div>
  
@@ -13,15 +33,21 @@
 <script>
 import HomeMoviesList from '@/components/HomeMoviesList.vue'
 import FilmService from '@/services/FilmService.js';
+import FilmList from '@/components/FilmList.vue';
+import FilmSuggestionList from '@/components/FilmSuggestionList.vue';
 export default {
   name: 'Home',
   components: {
-    HomeMoviesList
+    HomeMoviesList,
+    FilmList,
+    FilmSuggestionList
   },
   data() {
     return {
       filmsData: null,
       error: null,
+      filterName : '',
+      isClicked : false
     }
   },
   created () {
@@ -36,7 +62,28 @@ export default {
 
     
    
-  }
+  },
+  methods: {
+    setIsClicked(){
+      if(this.isClicked ==true) {
+        this.isClicked = false;
+      }
+      else {
+        this.isClicked = true;
+      }
+    }
+  },
+  computed: {
+    shouldClose() {
+      if(this.isClicked ==true || this.filterName.length < 3) {
+        return true;
+      } 
+      else {
+        return false;
+      }
+    }
+  },
+  
 }
 </script>
 
@@ -46,5 +93,12 @@ export default {
     margin:  auto;
     text-align: center;
   
+  }
+  .hidden {
+    display: none;
+  }
+  .movieData {
+
+    background-color: LightGray;
   }
 </style>
